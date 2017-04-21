19c1f79386439b7db7529e032b1d93c3bf22fe18
liulin1234 <doublel1234lbj@sina.com>
2017-04-21 15:17:51 +0800

    2017年4月21日15:17:12 响应式开发项目，虽然结果不行，但是过程是有的

---
+++ WebStormTrain/ResponsiveWeb/src/gulpfile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by doubleL on 2017/4/21.
+ */
+
+var gulp = require("gulp");
+var rev = require("gulp-rev");
+var revReplace = require("gulp-rev-replace");
+var useref = require("gulp-useref");
+var filter = require("gulp-filter");
+var csso = require("gulp-csso");
+
+gulp.task('defalut',fuction(){
+
+})
+
+
