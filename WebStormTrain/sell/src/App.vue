0dcc00f8783288709852c2ad13f96f1029e1d1c5
liulin1234 <doublel1234lbj@sina.com>
2017-04-25 17:53:16 +0800

    2017年4月25日17:52:54 data.json数据可用

--- WebStormTrain/sell/src/App.vue
+++ WebStormTrain/sell/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div class="header">
+      i am a header
+    </div>
+    <div class="tab">
+      i am a tab
+    </div>
+    <div class="content">
+      i am a content
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
