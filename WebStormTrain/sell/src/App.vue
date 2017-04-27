9d8a1d475a249766907f5f450e3f880610e6d4c4
liulin1234 <doublel1234lbj@sina.com>
2017-04-27 19:03:26 +0800

    2017年4月27日19:02:55 vue项目第5章done

--- WebStormTrain/sell/src/App.vue
+++ WebStormTrain/sell/src/App.vue
@@ -3,20 +3,38 @@
 
     <v-header></v-header>
 
-    <div class="tab">
-      <div class="tab-item">商品</div>
-      <div class="tab-item">评价</div>
-      <div class="tab-item">商家</div>
+    <div class="tab border-1px">
+      <router-link class="tab-item" to="/goods">商品</router-link>
+      <router-link class="tab-item" to="/ratings">评价</router-link>
+      <router-link class="tab-item" to="/seller">商家</router-link>
     </div>
-    <div class="content">
-      i am a content
-    </div>
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
   import header from './components/header/header.vue';
+
+  const ERR_OK = 0;
+
   export default {
+
+    data() {
+        return {
+            seller: {}
+        };
+    },
+    created() {
+      this.$http.get('/api/seller').then((response) => {
+          response = response.body;
+          console.log(response);
+          if (response.errno === ERR_OK) {
+              this.seller = response.data;
+              console.log(this.seller);
+          };
+      });
+    },
     components: {
       'v-header': header
     }
@@ -24,13 +42,25 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" >
+  @import "./common/stylus/mixin.styl";
+
+  /*&是父元素的意思*/
   #app
     .tab
       display:flex
       width:100%
       height:40px
       line-height:40px
+      /*border-bottom:1px solid rgba(7,17,27,0.1)*/
+      border-1px(rgba(7,17,27,0.1))
       .tab-item
         flex:1
         text-algin:center
+        font-size:14px
+        color:rgb(77,85,93)
+        &.active
+          color:rgb(240,20,20)
+
+
+
 </style>
