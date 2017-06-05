5f3de867e308ab1d6769c048a2b3f5455d94a875
liulin1234 <doublel1234lbj@sina.com>
2017-06-05 08:52:50 +0800

    2017年6月5日08:52:47

--- WebStormTrain/sell/src/components/goods/goods.vue
+++ WebStormTrain/sell/src/components/goods/goods.vue
@@ -160,7 +160,7 @@
   };
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" rel="stylesheet/stylus" >
 
   @import '../../common/stylus/mixin.styl';
 
---
+++ WebStormTrain/test/babel/lib/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by admin on 2017/5/24.
+ */
+var add = (a, b) => {
+  console.log(a + b);
+};
+add(1, 2);
